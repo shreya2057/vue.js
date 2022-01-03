cb31bd8eb32ff6accdd1a0ffc801edf83926f975
shreya2057 <shreya2057@gmail.com>
2022-01-03 20:50:30 +0545

    Vue.js Basic

--- vue.js
+++ vue.js
@@ -1,7 +1,30 @@
 const app = Vue.createApp({
-    data:function(){
-       return {
-        message: 'Option API Demo',
-       } 
+    data: function(){
+        return {
+            greetingMessage: "Hello, Welcome to my portfolio",
+            message: "My name is ",
+            name: "Shreya",
+            seen: true,
+            submitted: "Thank you for submitting",
+            alert: "Please submit the file"
+        }       
     },
+
+    methods:{
+        hidden(){
+            if(this.seen==true){
+                this.seen=false
+            }
+            else{
+                this.seen=true
+            }
+        }
+    },
+
+    computed: {
+        messages(){
+            return this.message + this.name
+        }
+    }
 })
+
